a974e6c70cff6c39f07b339e131545ccb7a66b6e
Nadeem Hasan <nhasan@nadmm.com>
2022-07-04 10:33:38 -0400

    Renamed class.

--- src/main/java/com/flightintel/app/tdes/DatisMessage.java
+++ src/main/java/com/flightintel/app/tdes/DatisMessage.java
@@ -1,23 +1,22 @@
 package com.flightintel.app.tdes;
 
-import com.jcraft.jzlib.ZStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.TimeZone;
 
-public class DAtisMessage {
-    private static final Logger logger = LoggerFactory.getLogger(DAtisMessage.class);
+public class DatisMessage {
+    private static final Logger logger = LoggerFactory.getLogger(DatisMessage.class);
 
     private final String icaoLocation;
     private final Timestamp issuedTimestamp;
@@ -25,9 +24,8 @@ public class DAtisMessage {
     private final String atisCode;
     private final String atisBody;
 
-    public DAtisMessage(final String xmlMessage) throws DAtisMessageParseException {
-        try {
-            ByteArrayInputStream stream = new ByteArrayInputStream(xmlMessage.getBytes("UTF-8"));
+    public DatisMessage(final String xmlMessage) throws DAtisMessageParseException {
+        try (ByteArrayInputStream stream = new ByteArrayInputStream(xmlMessage.getBytes(StandardCharsets.UTF_8))) {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
 
@@ -38,14 +36,13 @@ public class DAtisMessage {
             Element datisData = doc.getDocumentElement();
             icaoLocation = datisData.getElementsByTagName("airportID").item(0).getTextContent();
             String s = datisData.getElementsByTagName("DATISTime").item(0).getTextContent();
-            issuedTimestamp = new Timestamp(timestampFormater.parse(s).getTime());
+            issuedTimestamp = new Timestamp(Instant.now().toEpochMilli());
+            //new Timestamp(timestampFormater.parse(s).getTime());
             atisType = datisData.getElementsByTagName("editType").item(0).getTextContent();
             atisCode = datisData.getElementsByTagName("atisCode").item(0).getTextContent();
             atisBody = datisData.getElementsByTagName("dataBody").item(0).getTextContent();
-
-            stream.close();
         } catch (Exception e) {
-            throw new DAtisMessageParseException("Failed to create DAtis message due to: "+e.getMessage(), e);
+            throw new DAtisMessageParseException("Failed to create DAtis message due to: " + e.getMessage(), e);
         }
     }
 
